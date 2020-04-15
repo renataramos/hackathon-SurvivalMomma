9450ecd2a009eaa0a307c58b9dd0dac2632ded18
Miguel Lemos <mimiguelsl65@gmail.com>
2020-04-15 22:04:34 +0100

    add user services

--- src/main/java/org/academiadecodigo/rhashtafaris/kakathon/services/UserServiceImpl.java
+++ src/main/java/org/academiadecodigo/rhashtafaris/kakathon/services/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package org.academiadecodigo.rhashtafaris.kakathon.services;
 
 import org.academiadecodigo.rhashtafaris.kakathon.exceptions.UserNotFoundException;
+import org.academiadecodigo.rhashtafaris.kakathon.persistence.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
