3771d3aa950097b07e28156aebc2db19715d8264
Miguel Lemos <mimiguelsl65@gmail.com>
2020-04-16 01:55:04 +0100

    add update on controller

--- src/main/java/org/academiadecodigo/rhashtafaris/kakathon/controllers/TestController.java
+++ src/main/java/org/academiadecodigo/rhashtafaris/kakathon/controllers/TestController.java
@@ -2,22 +2,12 @@ package org.academiadecodigo.rhashtafaris.kakathon.controllers;
 
 import org.academiadecodigo.rhashtafaris.kakathon.converters.UserDtoToUser;
 import org.academiadecodigo.rhashtafaris.kakathon.converters.UserToUserDto;
-import org.academiadecodigo.rhashtafaris.kakathon.dto.UserDto;
-import org.academiadecodigo.rhashtafaris.kakathon.persistence.model.User;
 import org.academiadecodigo.rhashtafaris.kakathon.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class TestController {
@@ -41,11 +31,11 @@ public class TestController {
         this.userDtoToUser = userDtoToUser;
     }
 
-    @GetMapping("/")
+    @RequestMapping(method = RequestMethod.GET, path = {"/", ""})
     public ModelAndView userList () {
 
         //model.addAttribute("user", userService.listUsers());
 
-        return new ModelAndView("test");
+        return new ModelAndView("index");
     }
 }
