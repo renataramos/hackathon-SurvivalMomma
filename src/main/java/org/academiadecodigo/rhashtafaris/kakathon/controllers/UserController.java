626779e1d5ae149cc93889558890b53d918b42fa
Flusko <luscofusco@gmail.com>
2020-04-16 12:54:51 +0100

    user controller

--- src/main/java/org/academiadecodigo/rhashtafaris/kakathon/controllers/UserController.java
+++ src/main/java/org/academiadecodigo/rhashtafaris/kakathon/controllers/UserController.java
@@ -1,7 +1,49 @@
 package org.academiadecodigo.rhashtafaris.kakathon.controllers;
 
+import org.academiadecodigo.rhashtafaris.kakathon.converters.UserDtoToUser;
+import org.academiadecodigo.rhashtafaris.kakathon.converters.UserToUserDto;
+import org.academiadecodigo.rhashtafaris.kakathon.persistence.model.User;
+import org.academiadecodigo.rhashtafaris.kakathon.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
+    private UserService userService;
+
+    private UserDtoToUser userDtoToUser;
+    private UserToUserDto userToUserDto;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    public void setUserDtoToUser(UserDtoToUser userDtoToUser) {
+        this.userDtoToUser = userDtoToUser;
+    }
+
+    @Autowired
+    public void setUserToUserDto(UserToUserDto userToUserDto) {
+        this.userToUserDto = userToUserDto;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public String showUser(@PathVariable Integer id, Model model) throws Exception {
+
+        User user = userService.get(id);
+
+        model.addAttribute("user", userToUserDto.convert(user));
+
+        return "user/show";
+    }
 
 
 }
